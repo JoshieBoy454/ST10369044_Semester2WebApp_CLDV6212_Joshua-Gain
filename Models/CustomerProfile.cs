abc5d4ac3258922bdf27163d2f1835cf7826250f
Joshua Gain <ST10369044@vcconnect.edu.za>
2024-08-29 21:46:31 +0200

    Add project files.

---
+++ Models/CustomerProfile.cs
@@ -0,0 +1,23 @@
+﻿using Azure;
+using Azure.Data.Tables;
+
+namespace ST10369044Semerster2WebApp.Models
+{
+    public class CustomerProfile:ITableEntity
+    {
+        public string PartitionKey { get; set; }
+        public string RowKey { get; set; }
+        public DateTimeOffset? Timestamp { get; set; }
+        public ETag ETag { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public CustomerProfile()
+        {
+            PartitionKey = "CustomerProfile";
+            RowKey = Guid.NewGuid().ToString();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+using ST10369044Semerster2WebApp.Services;
+
+namespace ST10369044Semerster2WebApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            builder.Services.AddSingleton<BlobService>();
+            builder.Services.AddSingleton<QueueService>();
+            builder.Services.AddSingleton<TableService>();
+            builder.Services.AddSingleton<FileService>();
+
+            var app = builder.Build();
+
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseRouting();
+            app.UseAuthorization();
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+            app.Run();
+        }
+    }
+}
